24cac1ba59ba274aa3cad4d42ef85411e3fa26d3
mzaini30 <muhzaini30@gmail.com>
2018-02-18 16:49:30 +0000

    oke

--- data.js
+++ data.js
@@ -12,7 +12,23 @@ blog = [{
 	'nama': 'Zen',
 	'deskripsi': 'Senyumlah :)'
 }, {
-	'link': 'http://muhammadzaini.com',
-	'nama': 'Zen',
-	'deskripsi': 'Senyumlah :)'
+	'link': 'http://dewieajaa.blogspot.co.id',
+	'nama': 'Dewie Dean',
+	'deskripsi': 'Ada kekuatan di balik kesederhanaan'
+}, {
+	'link': 'http://www.henipuspita.net',
+	'nama': 'Heni Puspita',
+	'deskripsi': 'Catatan mama Rayyaan Razqa'
+}, {
+	'link': 'http://japanesestation.com',
+	'nama': 'Japanese Station',
+	'deskripsi': 'Portal berita Jepang'
+}, {
+	'link': 'http://www.mysatnite.com',
+	'nama': 'My Satnite',
+	'deskripsi': 'Your story is our story'
+}, {
+	'link': 'http://teknokanz.blogspot.co.id',
+	'nama': 'Si Kanz Technologies',
+	'deskripsi': 'Your story is our story'
 }];
