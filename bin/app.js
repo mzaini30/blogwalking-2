a2323faf02960b4801848b1a119ef4e7b10a34bd
mzaini30 <muhzaini30@gmail.com>
2018-02-18 16:26:35 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -1,3 +1,12 @@
 jQuery(function($){
 	$('.tampil-blog').height($(window).height() - $('.credit').height());
+
+	var intQuoteCount = blog.length; // The number of quotes in your library
+	var dtNow = new Date();
+	var intTZOffset = dtNow.getTimezoneOffset() * 60000; // automatically adjust for user timezone
+	var intNow = dtNow.getTime() - intTZOffset;
+	var intDay = Math.floor(intNow / 86400000); // The number of 'local' days since Jan 1, 1970
+	var intQuoteToDisplay = intDay % intQuoteCount;
+
+	$('.tampil-blog').attr('src', blog[intQuoteToDisplay]);
 });
